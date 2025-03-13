9cff3bb206b42b289d7c87d4ebcf296b86c172c0
sinfirst <nikitakoz2006@gmail.com>
2025-03-13 22:29:57 +0300

    modified:   .vscode/settings.json 	modified:   Kozlovskiy_LR3_project.cpp 	modified:   Kozlovskiy_LR3_project.exe

--- Kozlovskiy_LR3_project.cpp
+++ Kozlovskiy_LR3_project.cpp
@@ -6,48 +6,50 @@ float readValues(){
     float value;
     cout<<"Введите число\n";
     cin>>value;
-    cin.ignore();
     return value;
 }
 
 int firstTask(float x, float y, float z){
+    cout<<x<<endl;
+    cout<<y<<endl;
+    cout<<z<<endl;
     return (int) x + y + z;
 }
 
 float secondTask(float x, float y, float z){
-    
+    return sqrt(fabs(x)*fabs(y)*fabs(z)) - floor(sqrt(fabs(x)*fabs(y)*fabs(z)));
 }
 
 int main(){
     int vibor;
     float x, y, z;
-    cout<<"Выберите действие\n";
-    cout<<"№1 Введение числа x\n";
-    cout<<"№2 Введение числа y\n";
-    cout<<"№3 Введение числа z\n";
-    cout<<"№4 Целая часть суммы 3-х чисел\n";
-    cout<<"№5 Дрбная часть среднего геометрического модулей 3-х чисел\n";
-    cout<<"Введите ноль чтобы завершить выполнение программы\n";
     while(vibor != 0)
     {
+        cout<<"Выберите действие\n";
+        cout<<"№1 Введение числа x\n";
+        cout<<"№2 Введение числа y\n";
+        cout<<"№3 Введение числа z\n";
+        cout<<"№4 Целая часть суммы 3-х чисел\n";
+        cout<<"№5 Дрбная часть среднего геометрического модулей 3-х чисел\n";
+        cout<<"Введите ноль чтобы завершить выполнение программы\n";
         cin>>vibor;
-        cin.ignore();
         switch(vibor){
         case 1:
             x = readValues();
             break;
         case 2:
-            x = readValues();
+            y = readValues();
             break;
         case 3:
-            x = readValues();
+            z = readValues();
             break;
         case 4:
-            cout<<firstTask(x, y, z);
+            cout<<firstTask(x, y, z)<<endl;
             break;
         case 5:
-            cout<<secondTask(x, y, z);
+            cout<<secondTask(x, y, z)<<endl;
             break;
         };
+    }
     return 0;
-}}
+}
